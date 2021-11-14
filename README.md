abc543c2ab8bde9b8414112cc6d7089d4c589db2
Ivo Lejon <ivo.lejon@gmail.com>
2021-11-14 15:03:39 +0100

    fix: updated readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# swedish-keywords
+# Requirements
+
+You need python version >=3.6
+
+# Installation
+
+Run `sh install.sh` to download language models and required python packages.
+This space necessary is approximately 2 gb.
+
+# Server
+
+Start with python `app.py`
+There are three endpoint:
+
+- `/all` - Gets all words `nouns`, `verbs`, `enteties`
+- `/nouns` - Gets all `nouns`
+- `/verbs` - Gets all `verbs`
+- `/enteties` - Gets all `enteties`
+
+## Test
+
+You can test and see that it all works by running `python test.py`
--- test.py
+++ test.py
@@ -2,18 +2,40 @@
 import machine
 
 text = """
-Inte sedan 1979 och 1993 har det varit så få sorkar i norra Sverige. I fjälltrakterna är dessutom lämlarna ovanligt få. I Västmanland är det däremot ett riktigt sorkår. Det visar resultaten från årets miljöövervakning av smågnagare.
+En bra svampsäsong letar sig många ut i skogen. Men svamparna vi plockar är bara toppen på isberget. Den som lär sig läsa svamparnas underjordiska atlas kan också förutspå var kantarellerna kommer att titta fram.
 
-Sork och lämmel är en basföda för rovfåglar och ugglor och en av arterna som påverkas av årets små populationer i fjälltrakterna är pärlugglan.
+På flera håll i landet är det ett riktigt karljohanår i år och bilderna på fulla svampkorgar är många i sociala medier. Anders Dahlberg, professor i mykologi vid Sveriges lantbruksuniversitet, tror att det handlar om instinkten att samla kombinerat med en tävlingsinstinkt eller känsla av skattjakt.
 
-– I Ammarnäs och Vindelnområdet bedriver vi också miljöövervakning av pärlugglor, där vi inventerar ugglornas häckningsframgång i holkar. Som förväntat har vårens låga tätheter av lämlar och sorkar resulterat i mycket dålig häckningsframgång för ugglorna i dessa områden, säger Frauke Ecke, ansvarig för övervakningen av smågnagare, i ett pressmeddelande.
+– Man kan bli duktigare på att hitta svamp, det är absolut lite av en sport. Jag har också sett att intresset för svamp har vuxit väldigt de senaste åren. Till den grundkurs i mykologi som jag håller i på universitetet har vi 500 sökande till 30 platser.
 
-Sveriges nationella miljöövervakning av smågnagare utförs av Sveriges lantbruksuniversitet på uppdrag av Naturvårdsverket. En vecka efter midsommar inventeras fjällämlar i tre områden i Vålådalen/Ljungdalen, Ammarnäs och Stora Sjöfallet. Sork inventeras även i bland annat Vindelnområdet i Västerbotten och i Västmanland. Under fältinventeringen i fjällområdena påträffades inte en enda fjällämmel.
+I boken ”Svamparnas förunderliga liv” går han tillsammans med journalisten Anna Froster igenom naturens kanske viktigaste kugge – svamparna. De tar kantarellen, karljohansvampen, röksvampen och ytterligare ett antal svampar till hjälp för att visa på det enorma arbete som pågår bakom kulisserna – under jorden. Det handlar om promiskuösa murklor och onda parasiter.
 
-– Senaste gången vi hade ett riktigt lämmelår var år 2011 och det ser tyvärr ut att dröja innan vi får uppleva ett riktigt lämmelår igen. Det låga antalet som vi ser nu är en naturlig del av vad vi kallar för sorkcykler. Att bygga upp en stor population från årets låga antal kommer att ta cirka tre år, säger Frauke Ecke.
+– Syns man inte så finns man inte, och det gäller i högsta grad i naturen. Vi ser träd och iögonfallande växter. Men det mikroskopiska livet som är så mycket mer sett till både biomassa och aktivitet, det behöver vi lära oss att se.
 
-Om det är ett sorkår eller inte beror alltså på var i landet vi befinner oss. I Västmanland är årets sorkpopulation den tredje största sedan övervakningen startade 1973. Sork- och lämmelpopulationerna påverkar inte bara ugglor och rovfåglar. Ett toppår för sork syns tydligt i till exempel rävstammen, både samma år och året efter. Under ett bra sorkår minskar också rävens predation på exempelvis hare, orre och tjäder.
-"""
-# text = "jag cyklar till london och berlin"
+– Jag skulle önska att fler fick en förståelse för svamparnas betydelse. Svamparna är allt annat än kusinen från landet som får armbåga sig in för att få vara med på ett hörn – de spelar en av huvudrollerna i naturen.
+
+Ett kaninhål ner i underlandet
+Anders DahlbergAnders Dahlberg, professor i mykologi. Foto: Johan Samuelsson
+Den viktigaste delen av svampen är inte det vi ser utan de många tunna, underjordiska trådar, hyfer, som tillsammans bildar svampens mycel. Mycelet är svampens kropp, som växer fram under jorden och bryter ner och äter det som kommer i dess väg.
+
+Anders Dahlberg beskriver varje svamphatt som ett kaninhål ner i underlandet, där varje kvadratmeter rymmer många hundratusentals meter svamptrådar, en fortfarande relativt outforskad värld. Vetenskapen känner i dag till ungefär fem procent av alla svamparter som finns.
+
+I ”Svamparnas förunderliga liv” berättar Anders Dahlberg om två brittiska forskare som i en vanlig mataffär köpte en påse torkad svamp från Kina och DNA-analyserade de 15 svampbitarna. Bitarna visade sig vara från tre olika arter – alla nya för vetenskapen.
+
+En relativt ny företeelse
+Anders Dahlberg och hans kollegor har använt liknande DNA-teknik för att analysera prover från 1 500 av de skogar som ingår i den så kallade Markinventeringen, som ska visa hur marken i svenska skogar mår. Varje matsked skogsjord rymmer omkring 100 svamparter och de har bland annat kunnat konstatera att Sveriges vanligaste svamp är den lilla nedbrytande Penicillium spinnulosum. Ovan mark är den vanligaste svampen en skinnsvamp och tillika mykorrhizasvamp, som syns som ett vitt ludd på undersidan av fallna träd.
+
+– Vi håller på att få en bra bild av vilka svampar som finns i marken och tittar samtidigt på hur deras förekomst korrelerar till skogens egenskaper, markens kolinlagring, hur träden har vuxit med mera. Det är kunskap som kan få stor betydelse för skogsskötseln, säger Anders Dahlberg.
+
+Möjligen hänger den begränsade kunskapen om svampar ihop med att det svampintresse som Anders Dahlberg vittnar om är en relativt ny företeelse. Länge var mykofobi, svampfobi, ett relevant begrepp för att beskriva svenskarnas förhållande till svamp. Carl von Linne kallade till exempel svamparna för en motbjudande röra och ”Floras strövande pack”. Så småningom blev det en delikatess för överklassen, men först på 1970-talet vaknade intresset för svampplockning på allvar.
+
+En sport som går att lära sig
+I dag lockar svampletandet allt fler, och det är en sport som går att lära sig, säger Anders Dahlberg. Men någon quickfix finns inte, det handlar om att läsa av träd, miljö och väderlek. Är det mykorrhizasvampar, som de allra flesta av våra matsvampar är, måste träden finnas där. Är det torrt letar man där det är fuktigt, har det regnat mycket går man där det vanligtvis är torrt.
+
+– Alla kan lära sig, absolut. Men det är komplext att förklara och svårt att ge konkreta, säkra råd. Det är som att headhunta till ett fotbollslag: var hittar jag talangerna? Miljön ska vara rätt, förutsättningarna ska vara rätt – då finns de där. Ibland.
+
+Det vi ger oss ut på jakt efter i höstsolen är svampens fruktkropp, den lilla del av svampen som ibland dyker upp ovan jord. Men för svampen spelar det ingen roll om de bildar fruktkroppar eller inte. Är inte väderleken den rätta väntar de bara – och de kan vänta länge.
+
+– Oavsett om vi hittar svamp eller inte så tror jag att svamparna är en bra ingång för att uppleva komplexiteten i naturen. Att lära sig mer om dem kan ge en fiktiv känsla av att man förstår hur det hänger ihop."""
 keywords = machine.run(text, 'all')
 print(keywords)
